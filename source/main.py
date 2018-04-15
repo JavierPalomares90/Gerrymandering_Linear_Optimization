94552977f0b26839803eb1cacaa02c9d99a9017a
ttillett13 <ttillett@utexas.edu>
2018-04-15 16:13:30 -0500

    updated organization and added placeholder function for network flow solving

--- source/main.py
+++ source/main.py
@@ -3,29 +3,47 @@
 
 # In[1]:
 
-
+from gurobipy import *
 import utils
-dataDir = "../census_data/"
-# file with population data
-populationFile = "/DEC_10_PL_P3_with_ann.csv"
-# file with geographical data
-geoFile = "/DEC_10_PL_G001_with_ann.csv"
-dirs = utils.getSubdirs(dataDir)
-blocks = []
-# get all of the blocks into one list
-for d in dirs:
-    block,popData,geoData = utils.getBlocks(dataDir + d + populationFile,dataDir + d + geoFile)
-    for b in block:
-        blocks.append(b)
-# read the political data
-fipsDataFile = "../census_data/st44_ri_cou.txt";
-poliDataFile = "../political_data/US_elect_county.csv";
-counties = utils.getPoliDataByCounty(poliDataFile,fipsDataFile);
+
+def readData():
+    dataDir = "../census_data/"
+    # file with population data
+    populationFile = "/DEC_10_PL_P3_with_ann.csv"
+    # file with geographical data
+    geoFile = "/DEC_10_PL_G001_with_ann.csv"
+    dirs = utils.getSubdirs(dataDir)
+    blocks = []
+    # get all of the blocks into one list
+    for d in dirs:
+        block,popData,geoData = utils.getBlocks(dataDir + d + populationFile,dataDir + d + geoFile)
+        for b in block:
+            blocks.append(b)
+    # read the political data
+    fipsDataFile = "../census_data/st44_ri_cou.txt"
+    poliDataFile = "../political_data/US_elect_county.csv"
+    counties = utils.getPoliDataByCounty(poliDataFile,fipsDataFile)
+    return (blocks, counties)
+
+
+def assign():
+    # gurobi network flow example: http://www.gurobi.com/documentation/7.5/examples/netflow_py.html
+    #
+    # cost function f(i,j) is the euclidian distance from block i to district j times population of block i
+    # we can create a cost matrix f with rows being blocks and columns being districts
+    # we can write the edges u as constrained to 0 or 1 indicating whether block i is assigned to district j
+    # 
+
+
+    # Create optimization model
+    m = Model('netflow')
+
+
+
 
 
 # In[5]:
-
-
+(blocks, counties) = readData()
 #This is a population block
 print(blocks[3])
 
