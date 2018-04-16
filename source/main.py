fc45b2338aa9e63fe89de4bde0c4edc5ed0fe004
Javier Palomares <javier.palomares.90@gmail.com>
2018-04-15 21:34:46 -0500

    Correct bad merge

--- source/main.py
+++ source/main.py
@@ -1,49 +1,36 @@
 
 # coding: utf-8
 
+# In[4]:
+
+
+# coding: utf-8
+
 # In[1]:
 
 from gurobipy import *
 import utils
 
 def readData():
-	dataDir = "../census_data/";
-	# file with population data
-	populationFile = "/DEC_10_PL_P3_with_ann.csv"
-	# file with geographical data
-	geoFile = "/DEC_10_PL_G001_with_ann.csv";
-	# file with the congressional districts per block
-	cdFile = "../cd115/National_CD115.txt"
-	dirs = utils.getSubdirs(dataDir)
-	blocks = []
-	# get all of the blocks into one list
-	for d in dirs:
-		block,popData,geoData = utils.getBlocks(dataDir + d + populationFile,dataDir + d + geoFile,cdFile)
-		for b in block:
-			blocks.append(b)
-	# read the political data
-	fipsDataFile = "../census_data/st44_ri_cou.txt";
-	poliDataFile = "../political_data/US_elect_county.csv";
-	counties = utils.getPoliDataByCounty(poliDataFile,fipsDataFile);
-    
-	dataDir = "../census_data/"
+    dataDir = "../census_data/";
     # file with population data
     populationFile = "/DEC_10_PL_P3_with_ann.csv"
     # file with geographical data
-    geoFile = "/DEC_10_PL_G001_with_ann.csv"
+    geoFile = "/DEC_10_PL_G001_with_ann.csv";
+    # file with the congressional districts per block
+    cdFile = "../cd115/National_CD115.txt"
     dirs = utils.getSubdirs(dataDir)
     blocks = []
     # get all of the blocks into one list
     for d in dirs:
-        block,popData,geoData = utils.getBlocks(dataDir + d + populationFile,dataDir + d + geoFile)
+        block,popData,geoData = utils.getBlocks(dataDir + d + populationFile,dataDir + d + geoFile,cdFile)
         for b in block:
             blocks.append(b)
     # read the political data
-    fipsDataFile = "../census_data/st44_ri_cou.txt"
-    poliDataFile = "../political_data/US_elect_county.csv"
-    counties = utils.getPoliDataByCounty(poliDataFile,fipsDataFile)
+    fipsDataFile = "../census_data/st44_ri_cou.txt";
+    poliDataFile = "../political_data/US_elect_county.csv";
+    counties = utils.getPoliDataByCounty(poliDataFile,fipsDataFile);
     return (blocks, counties)
-
 
 def assign():
     # gurobi network flow example: http://www.gurobi.com/documentation/7.5/examples/netflow_py.html
@@ -57,20 +44,11 @@ def assign():
     # Create optimization model
     m = Model('netflow')
 
-
-
-
-
-
 # In[5]:
 (blocks, counties) = readData()
 #This is a population block
 print(blocks[3])
-
-# In[2]:
-
-
-#who
 # This is a vote by county
 #print(counties[0])
+
 
