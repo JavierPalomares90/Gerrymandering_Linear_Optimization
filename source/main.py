04f6f312c4501707f92cf52340694dea6e48e190
ttillett13 <ttillett@utexas.edu>
2018-04-17 21:48:33 -0500

    some changes to objective function

--- source/main.py
+++ source/main.py
@@ -12,19 +12,24 @@ from gurobipy import *
 import utils
 from math import radians, sin, cos, acos
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+# install basemap with: sudo -H pip3 install https://github.com/matplotlib/basemap/archive/v1.1.0.tar.gz
+
 
 
 
 def readData():
-    dataDir = "../census_data/";
+    dataDir = "../census_data/"
     # file with population data
     populationFile = "/DEC_10_PL_P3_with_ann.csv"
     # file with geographical data
-    geoFile = "/DEC_10_PL_G001_with_ann.csv";
+    geoFile = "/DEC_10_PL_G001_with_ann.csv"
     # file with the congressional districts per block
     cdFile = "../cd115/National_CD115.txt"
     dirs = utils.getSubdirs(dataDir)
     blocks = []
+    counties = []
     # get all of the blocks into one list
     count = 0
     for d in dirs:
@@ -33,11 +38,11 @@ def readData():
             blocks.append(b)
             count = count + 1
             if count > 6:
-                break
+                return (blocks, counties)
     # read the political data
-    fipsDataFile = "../census_data/st44_ri_cou.txt";
-    poliDataFile = "../political_data/US_elect_county.csv";
-    counties = utils.getPoliDataByCounty(poliDataFile,fipsDataFile);
+    fipsDataFile = "../census_data/st44_ri_cou.txt"
+    poliDataFile = "../political_data/US_elect_county.csv"
+    #counties = utils.getPoliDataByCounty(poliDataFile,fipsDataFile)
     return (blocks, counties)
 
 
@@ -51,6 +56,9 @@ def getDistance(block, district):
     return dist
 
 
+def connected(start, end):
+    return True
+
 def getCostsArcsCapacity(blocks, districts):
     f = np.empty((len(blocks), len(districts)))
     arcs = tuplelist()
@@ -58,15 +66,38 @@ def getCostsArcsCapacity(blocks, districts):
     capacity = {}
     totalPop = 0
     for i,block in enumerate(blocks):
-        totalPop += block["Population"]
-        for j,district in enumerate(districts):
-            f[i,j] = getDistance(block, district)
-            arc = (block["Block"], j)
-            arcs.append(arc)
-            numeric_Arcs.append((i,j))
-            capacity[arc] = block["Population"]
-    return (f, arcs, capacity, numeric_Arcs, totalPop)
+    #     totalPop += block_start["Population"]
+    #     for j,block_end in enumerate(blocks):
+    #         if block_start is not block_end:
+    #             if connected(block_start, block_end):
+    #                 arc = (i, j)
+    #                 arcs.append(arc)
+    #                 #capacity[arc] = block["Population"]
 
+        for j, district in enumerate(districts):
+            f[i, j] = getDistance(block, district)
+            arc = (i, j)
+            #arc = (block["Block"], j)
+            arcs.append(arc)
+            #numeric_Arcs.append((i, j))
+            capacity[arc] = block["Population"]
+    return (f, arcs, capacity, totalPop)
+
+
+def drawMap(solution, blocks, n, m):
+    fig = plt.figure(figsize=(8, 8))
+    map = Basemap(projection='merc', resolution='h',
+                  width=5E1, height=5E1,
+                  lat_0=40, lon_0=-70, )
+    #map.etopo(scale=0.5, alpha=0.5)
+    for j in range(m):
+        for i in range(n):
+            if solution[i, j] > 0:
+                block = blocks[i]
+                x, y = map(float(block["Longitude"]), float(block["Latitude"]))
+                plt.plot(x, y, 'ok', markersize=5)
+                plt.text(x, y, str(j), fontsize=12)
+    plt.show()
 
 
 def assign(blocks, districts, counties):
@@ -77,15 +108,16 @@ def assign(blocks, districts, counties):
     # we can create a cost matrix f with rows being blocks and columns being districts
     # we can write the edges u as constrained to 0 or 1 indicating whether block i is assigned to district j
     #
-    (f, arcs, capacity, numericArcs, totalPop) = getCostsArcsCapacity(blocks, districts)
-    print(f)
-    print(capacity)
+    (f, arcs, capacity, totalPop) = getCostsArcsCapacity(blocks, districts)
+    #print(f)
+    #print(capacity)
 
     n = len(blocks)
     m = len(districts)
-    pop_lower = 0.3 * totalPop
-    pop_upper = 0.7 * totalPop
-    M = 1000
+    print(totalPop)
+    pop_lower = 0.1 * totalPop
+    pop_upper = 0.9 * totalPop
+    M = 1000 # should be as large as the largest block population. might calculate dynamically
 
 
     # Create optimization model
@@ -96,8 +128,12 @@ def assign(blocks, districts, counties):
     indic = model.addVars(n, m, vtype=GRB.INTEGER, lb=0, ub=1, name="indic")
 
 
+    # d[i,j] * population of block i * indic[i,j]
+    model.setObjective((quicksum(f[i,j] * blocks[i]["Population"] * indic[i,j] for i in range(n) for j in range(m))), GRB.MINIMIZE)
+    #model.setObjective((f[i,j] * blocks[i]["Population"] for i in range(n) for j in range(m)), GRB.MINIMIZE)
+
     # Arc capacity constraints
-    model.addConstrs((flow.sum('*', i, j) <= capacity[i, j] for i, j in arcs), "cap")
+    #model.addConstrs((flow.sum('*', i, j) <= capacity[i, j] for i, j in arcs), "cap")
 
     # to ensure all blocks are allocated and no splitting allowed
     model.addConstrs( (flow.sum(i,'*') == blocks[i]["Population"] for i in range(n)), "node")
@@ -126,6 +162,7 @@ def assign(blocks, districts, counties):
                     totalPop += solution[i,j]
             print("For district " + str(j) + ": the population is " + str(totalPop)
                  + " and the assigned blocks are " + str(blocks_assigned) + "\n")
+        drawMap(indic_sol, blocks, n, m)
 
 
 # In[5]:
@@ -138,7 +175,7 @@ print(blocks[3])
 
 
 # In[2]:
-u = utils.getNumBlockInDistrict(blocks)
+#u = utils.getNumBlockInDistrict(blocks)
 
 districts = [{'Latitude': '+41.1879323', 'Longitude': '-071.5828012'},
              {'Latitude': '+41.1686180', 'Longitude': '-071.5928347'}]
